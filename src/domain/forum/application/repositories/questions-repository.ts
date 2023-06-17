030d4672a175c9d86f3d6935cc94eab8cbe580e4
LetsWorkb1 <jhow.win@gmail.com>
2023-06-16 23:30:39 -0300

    Create: create of fetch most recent question and create fetch answer of one question

--- src/domain/forum/application/repositories/questions-repository.ts
+++ src/domain/forum/application/repositories/questions-repository.ts
@@ -1,8 +1,10 @@
+import { PaginationParms } from '@/core/repositories/pagination-params'
 import { Question } from '../../enterprise/entities/question'
 
 export interface QuestionsRepository {
   findBySlug(slug: string): Promise<Question | null>
   findById(id: string): Promise<Question | null>
+  findManyRecent(parms: PaginationParms): Promise<Question[]>
   delete(question: Question): Promise<void>
   save(question: Question): Promise<void>
   create(question: Question): Promise<void>
